551104418b45defc6dd7a29a42a776b8368186d3
Abhishek Sharma <Abhishek.Sharma@Yodlee.com>
2019-08-19 23:31:26 +0530

    remove note functionality added

--- notes.js
+++ notes.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-
+const chalk = require('chalk')
 const getNotes = function () {
     return 'Your notes...'
 }
@@ -16,10 +16,23 @@ const addNote = function (title, body) {
             body: body
         })
         saveNotes(notes)
-        console.log('New note added!')
+        console.log(chalk.green.inverse('New note added!'))
     } else {
-        console.log('Note title taken!')
+        console.log(chalk.red.inverse('Note title taken!'))
     }
+}
+const removeNote = function(title){
+    const notes = loadNotes()
+    const notestoKeep = notes.filter(function(note){
+        return note.title !== title
+    })
+    if (notes.length > notestoKeep.length) {
+        console.log(chalk.green.inverse('Note Removed!'))
+        saveNotes(notestoKeep)    
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }
+    
 }
 
 const saveNotes = function (notes) {
@@ -37,7 +50,11 @@ const loadNotes = function () {
     }
 }
 
+
+
+
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote:removeNote
 }
